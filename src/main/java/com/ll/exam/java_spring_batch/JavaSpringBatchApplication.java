2c863156766435ecd9ccee177055eaa3979426e8
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-09-22 10:12:48 +0900

    스프링부트앱을 배치앱으로 설정하여, 관련스키마가 생기도록

--- src/main/java/com/ll/exam/java_spring_batch/JavaSpringBatchApplication.java
+++ src/main/java/com/ll/exam/java_spring_batch/JavaSpringBatchApplication.java
@@ -1,9 +1,11 @@
 package com.ll.exam.java_spring_batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class JavaSpringBatchApplication {
 
 	public static void main(String[] args) {
