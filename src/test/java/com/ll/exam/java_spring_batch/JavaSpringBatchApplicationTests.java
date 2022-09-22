fe2d1b15df3b0007990630e56fb7be3f3cfd6253
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-09-22 09:25:38 +0900

    세팅

---
+++ src/test/java/com/ll/exam/java_spring_batch/JavaSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.java_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
